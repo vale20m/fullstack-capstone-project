3127fc85e9b165492d39c38118097cc809abe0bb
vale20m <valentinveintemilla@gmail.com>
2024-06-08 14:58:06 -0400

    4.3 DONE

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -65,4 +65,54 @@ router.post('/register', async (req, res) => {
 });
 
 
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+        
+        // Task 2: Access MongoDB `users` collection
+        const collection = db.collection("users");
+        
+        // Task 3: Check for user credentials in database
+        const theUser = await collection.findOne({'email': req.body.email});
+        
+        // Task 4: Task 4: Check if the password matches the encrypyted password and send appropriate message on mismatch
+        if (theUser) {
+            let result = await bcryptjs.compare(req.body.password, theUser.password);
+            
+            if(!result) {
+                logger.error('Passwords do not match');
+                return res.status(404).json({ error: 'Wrong pasword' });
+            }
+
+            // Task 5: Fetch user details from database
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+
+            
+            // Task 6: Create JWT authentication if passwords match with user._id as payload
+            let payload = {
+                user: {
+                    id: theUser._id.toString(),
+                },
+            };
+            const authtoken = jwt.sign(payload, JWT_SECRET);
+            logger.error('User logged in successfully');
+            res.json({authtoken, userName, userEmail });
+        } else {
+
+            // Task 7: Send appropriate message if user not found
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }      
+    
+    } catch (e) {
+
+        logger.error(e);
+        return res.status(500).send('Internal server error');
+
+    }
+});
+
+
 module.exports = router;
