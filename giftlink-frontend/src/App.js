227ddd41e8e3d10e711807964994cdf219a83504
vale20m <valentinveintemilla@gmail.com>
2024-06-08 19:16:42 -0400

    4.6 DONE

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -8,6 +8,7 @@ import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import SearchPage from './components/SearchPage/SearchPage';
+import Profile from './components/Profile/Profile';
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
             <Route path="/app/register" element={<RegisterPage />} />
             <Route path="/app/product/:productId" element={<DetailsPage/>} />
             <Route path="/app/search" element={<SearchPage/>} />
+            <Route path="/app/profile" element={<Profile/>} />
         </Routes>
         </>
   );
